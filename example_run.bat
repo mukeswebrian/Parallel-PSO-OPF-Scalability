f5a897396a479dfa91bc78b556dda2c43201d17a
Brian Mukeswe <mukeswebrian@yahoo.com>
2020-11-12 00:03:22 -0800

    added run plan

--- example_run.bat
+++ example_run.bat
@@ -19,11 +19,11 @@
 ::python pso_algorithm.py case14 50 180 0.25 0.2 0.7 particles180run3
 
 
-python pso_algorithm_profiled.py "" 50 20 0.25 0.2 0.7 particles20run1
-python pso_algorithm_profiled.py "" 50 60 0.25 0.2 0.7 particles60run1
-python pso_algorithm_profiled.py "" 50 100 0.25 0.2 0.7 particles100run1
+::python pso_algorithm_profiled.py "" 50 20 0.25 0.2 0.7 particles20run1
+::python pso_algorithm_profiled.py "" 50 60 0.25 0.2 0.7 particles60run1
+::python pso_algorithm_profiled.py "" 50 100 0.25 0.2 0.7 particles100run1
 python pso_algorithm_profiled.py "" 50 140 0.25 0.2 0.7 particles140run1
-python pso_algorithm_profiled.py "" 50 180 0.25 0.2 0.7 particles180run1
+::python pso_algorithm_profiled.py "" 50 180 0.25 0.2 0.7 particles180run1
 
 
 pause
